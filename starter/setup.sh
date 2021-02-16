fca73a6bd886d4c1efb49f5ab89e306675ef5a51
Ana Gabriele <57578764+Anagabsoares@users.noreply.github.com>
2021-02-15 16:10:16 -0800

    commit

--- starter/setup.sh
+++ starter/setup.sh
@@ -1,1 +1,1 @@
-export DATABASE_URL="postgres://mrbsqzdwocnpiz:47ee0ccfd1e997cc136a0d6ece9b350ff1eaf1dfb554776b95898dd68beeb575@ec2-35-174-118-71.compute-1.amazonaws.com:5432/d91b6rhb3gpork"
+ DATABASE_URL
